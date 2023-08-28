a32338d18a9f16920b618191d1daac693d197f7d
laxminara1996 <laxminara1996@gmail.com>
2023-08-28 19:59:17 +0530

    initial commit

---
+++ src/main/java/in/laxmi/SpringJdbcSecurityAuthenticationAppApplication.java
@@ -0,0 +1,13 @@
+package in.laxmi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJdbcSecurityAuthenticationAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJdbcSecurityAuthenticationAppApplication.class, args);
+	}
+
+}
